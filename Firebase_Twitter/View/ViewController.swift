52a3dc13ff81f327ae9f0d322b7f4dbbb37b99a3
Raees Nawaj Shaikh <raeesshaikh4222@gmail.com>
2023-12-28 13:09:39 +0530

    created new branch due to fatal error

---
+++ Firebase_Twitter/View/ViewController.swift
@@ -0,0 +1,121 @@
+    //
+    //  ViewController.swift
+    //  Firebase_Twitter
+    //
+    //  Created by Vishal on 08/12/23.
+    //
+
+    import UIKit
+    import Firebase
+    import FirebaseFirestore
+    import FirebaseAuth
+
+    class ViewController: UIViewController, ProfileViewModelDelegate {
+        
+        @IBOutlet var profileImg: UIImageView!
+        @IBOutlet var nameLabel: UILabel!
+        @IBOutlet var emailLabel: UILabel!
+        @IBOutlet var followersCount: UILabel!
+        @IBOutlet var followingCount: UILabel!
+        @IBOutlet var followBTN: UIButton!
+        @IBOutlet var commentBTN: UIButton!
+        
+        var viewModel: ProfileViewModel!
+        var profileUserID: String?
+        
+        var loggedInUserID: String?
+        var selectedUserID: String?
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            signIn()
+            viewModel = ProfileViewModel()
+            viewModel.delegate = self
+            print("profileUserID: \(profileUserID)")
+            print("loggedInUserID: \(loggedInUserID)")
+            print("selectedUserID : \(selectedUserID)")
+            loadUserProfile()
+        }
+        
+        func loadUserProfile(completion: (() -> Void)? = nil){
+            guard let selectedUserID = selectedUserID else {
+                    print("loadUserProfile selectedUserID is nil")
+                    return
+                }
+                profileUserID = selectedUserID
+            viewModel.setUpForSelectedUser(profileUserId: selectedUserID){
+                completion?()
+            }
+        }
+        
+        func didUpdateProfile(_ profileModel: ProfileModel) {
+            print("didUpdateProfile called with profileModel: \(profileModel)")
+            DispatchQueue.main.async { [weak self] in
+                        guard let self = self else { return }
+                        
+                        self.nameLabel.text = profileModel.name ?? ""
+                        self.emailLabel.text = profileModel.email ?? ""
+                        
+                        // Handle follower and following counts
+                        self.followersCount.text = "\(profileModel.followers?.count ?? 0)"
+                        self.followingCount.text = "\(profileModel.following?.count ?? 0)"
+                        
+                        let title = profileModel.isFollowing == true ? "Following" : "Follow"
+                        self.followBTN.setTitle(title, for: .normal)
+                    }
+        }
+        
+        
+        @IBAction func backBtn(_ sender: UIButton) {
+            self.navigationController?.popViewController(animated: true)
+        }
+        
+        @IBAction func SignOutBtn(_ sender: UIButton) {
+            do {
+                try Auth.auth().signOut()
+                print("User signed out successfully")
+                
+                // Redirecting to LoginViewController after sign OUT
+                let loginVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "LogInViewController") as! LogInViewController
+                let appDelegate = UIApplication.shared.delegate as! AppDelegate
+                appDelegate.window?.rootViewController = loginVC
+                
+            } catch let signOutError as NSError {
+                print("Error signing out: \(signOutError)")
+            }
+        }
+        
+        @IBAction func followBTN(_ sender: UIButton) {
+            print("Follow button tapped")
+            print("ProfileUserID: \(profileUserID ?? "nil")")
+            guard let profileUserID = self.profileUserID else {
+                print("ProfileUserID is nil. Cannot follow.")
+                return
+            }
+            print("Follow button tapped for ProfileUserID: \(profileUserID)")
+            viewModel.followButtonTapped(profileUserID: profileUserID)
+        }
+        
+        @IBAction func commentBTN(_ sender: UIButton) {
+            presentCommentViewController()
+        }
+        
+        func signIn() {
+            if let user = Auth.auth().currentUser {
+                loggedInUserID = user.uid
+                print("User authenticated.")
+            } else {
+                print("User not authenticated.")
+            }
+        }
+    }
+
+    extension ViewController {
+        func presentCommentViewController() {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let commentVC = storyboard.instantiateViewController(withIdentifier: "CommentViewController") as! CommentViewController
+            commentVC.modalPresentationStyle = .overCurrentContext
+            commentVC.profileUserId = profileUserID
+            self.present(commentVC, animated: true, completion: nil)
+        }
+    }
--- Firebase_Twitter/ViewModel/ProfileViewModel.swift
+++ Firebase_Twitter/ViewModel/ProfileViewModel.swift
@@ -1,7 +1,7 @@
 ////
 ////  ProfileViewModel.swift
 ////  Firebase_Twitter
-////
+//// Success before github adding
 ////  Created by Vishal on 22/12/23.
 
 import Foundation
@@ -19,118 +19,110 @@ class ProfileViewModel {
     private let db = Firestore.firestore()
     private var profileCache: [String: ProfileModel] = [:]
     var isProfileLoaded: Bool = false
-
-    // MARK: - Authentication
-
-    func signIn(completion: @escaping () -> Void) {
-        // Authentication logic goes here
-        completion()
-    }
-
+    
+    
     // MARK: - Profile Loading
-
-    func loadProfile(profileUserID: String?, completion: @escaping () -> Void) {
+    
+    func loadProfile(profileUserID: String?, completion: (() -> Void)? = nil) {
         guard let profileUserID = profileUserID else {
             print("Profile user ID not available.")
             return
         }
-
+        
         // Check if the profile is already in the cache
         if let cachedProfile = profileCache[profileUserID] {
             self.profileModel = cachedProfile
             self.delegate?.didUpdateProfile(cachedProfile)
+            completion?()
             return
         }
-
+        
         db.collection("users").whereField("UUID", isEqualTo: profileUserID).getDocuments { [weak self] (querySnapshot, error) in
-            guard let self = self, let document = querySnapshot?.documents.first else {
+            guard let self = self else { return }
+            
+            if let error = error {
+                print("Error loading profile: \(error.localizedDescription)")
+                completion?()
                 return
             }
-
+            
+            guard let document = querySnapshot?.documents.first else {
+                print("No profile document found for user ID: \(profileUserID)")
+                completion?()
+                return
+            }
             let name = document.data()["name"] as? String ?? ""
             let email = document.data()["email"] as? String ?? ""
-
+            
             self.profileModel.name = name
             self.profileModel.email = email
-            self.profileModel.followers = []
-            self.profileModel.following = []
-
+            self.profileModel.followers = document.data()["followers"] as? [String]
+            self.profileModel.following = document.data()["following"] as? [String]
+            
+            
             print("loadProfile: name = \(self.profileModel.name), email: \(self.profileModel.email)")
-
+            
             // Cache the loaded profile
             self.profileCache[profileUserID] = self.profileModel
-
+            
             DispatchQueue.main.async {
                 self.delegate?.didUpdateProfile(self.profileModel)
-                completion()
+                completion?()
+                
             }
         }
     }
-
-    // MARK: - Profile Setup
-
-    func setupForSelectedUser(profileUserID: String) {
-        isProfileLoaded = false
-        loadProfile(profileUserID: profileUserID) { [weak self] in
-            guard let self = self else { return }
-            self.loadFollowerFollowingCounts(profileUserID: profileUserID)
-
-            if let loggedInUserID = Auth.auth().currentUser?.uid {
-                self.checkAndSetFollowStatus(loggedInUserID: loggedInUserID, profileUserID: profileUserID)
-            } else {
-                print("User is not logged in.")
-            }
-        }
-    }
-
+    
+    
+    
     // MARK: - Follow Button
-
+    
     func followButtonTapped(profileUserID: String?) {
         print("followButtonTapped called")
-
+        
         guard let profileUserID = profileUserID else { return }
-
+        
         if !isProfileLoaded {
             updateFollowStatus(loggedInUserID: Auth.auth().currentUser?.uid, profileUserID: profileUserID)
             isProfileLoaded = true
         }
     }
-
+    
     // MARK: - Follower/Following Counts
-
+    
     func loadFollowerFollowingCounts(profileUserID: String?) {
         guard let profileUserID = profileUserID else { return }
         let userDocRef = db.collection("users").document(profileUserID)
-
+        
         userDocRef.getDocument { [weak self] document, error in
             guard let self = self, let document = document else { return }
-
+            
             if let followersCount = document.data()?["followers"] as? [String],
-                let followingCount = document.data()?["following"] as? [String] {
-
+               let followingCount = document.data()?["following"] as? [String] {
+                
                 self.profileModel.name = document.data()?["name"] as? String ?? ""
                 self.profileModel.email = document.data()?["email"] as? String ?? ""
                 self.profileModel.followers = followersCount
                 self.profileModel.following = followingCount
-
+                
                 print("loadFollowerFollowingCounts followers: \(self.profileModel.followers), following: \(self.profileModel.following)")
-
+                
                 self.delegate?.didUpdateProfile(self.profileModel)
             } else {
                 print("Error in data format in user document.")
             }
         }
     }
-
+    
     // MARK: - Follow Status
-
+    
     func checkAndSetFollowStatus(loggedInUserID: String?, profileUserID: String?) {
         guard let loggedInUserID = loggedInUserID, let profileUserID = profileUserID else { return }
         let userDocRef = db.collection("users").document(profileUserID)
-
+        
         userDocRef.getDocument { [weak self] document, error in
             guard let self = self, let document = document else { return }
-
+            
             if let profileFollowers = document.data()?["followers"] as? [String] {
                 self.profileModel.isFollowing = profileFollowers.contains(loggedInUserID)
                 self.delegate?.didUpdateProfile(self.profileModel)
@@ -139,16 +131,16 @@ class ProfileViewModel {
             }
         }
     }
-
+    
     func updateFollowStatus(loggedInUserID: String?, profileUserID: String?) {
         guard let loggedInUserID = loggedInUserID, let profileUserID = profileUserID else { return }
         let userDocRef = db.collection("users").document(profileUserID)
-
+        
         userDocRef.getDocument { [weak self] document, error in
             guard let self = self, let document = document else { return }
-
+            
             var profileFollowers = document.data()?["followers"] as? [String] ?? []
-
+            
             if profileFollowers.contains(loggedInUserID) {
                 profileFollowers.removeAll { $0 == loggedInUserID }
                 self.profileModel.isFollowing = false
@@ -156,7 +148,7 @@ class ProfileViewModel {
                 profileFollowers.append(loggedInUserID)
                 self.profileModel.isFollowing = true
             }
-
+            
             userDocRef.updateData(["followers": profileFollowers]) { error in
                 if let error = error {
                     print("Error updating profile followers: \(error.localizedDescription)")
@@ -168,24 +160,24 @@ class ProfileViewModel {
             }
         }
     }
-
+    
     // MARK: - Logged-In User Following
-
+    
     func updateLoggedInUserFollowing(loggedInUserID: String?, profileUserID: String?) {
         guard let loggedInUserID = loggedInUserID else { return }
         let loggedInUserDocRef = db.collection("users").document(loggedInUserID)
-
+        
         loggedInUserDocRef.getDocument { document, error in
             guard let document = document else { return }
-
+            
             var loggedInUserFollowing = document.data()?["following"] as? [String] ?? []
-
+            
             if self.profileModel.isFollowing ?? false {
                 loggedInUserFollowing.append(profileUserID!)
             } else {
                 loggedInUserFollowing.removeAll { $0 == profileUserID }
             }
-
+            
             loggedInUserDocRef.updateData(["following": loggedInUserFollowing]) { error in
                 if let error = error {
                     print("Error updating logged-in user's following: \(error.localizedDescription)")
@@ -195,4 +187,23 @@ class ProfileViewModel {
             }
         }
     }
+    
+
+    func setUpForSelectedUser(profileUserId: String,completion: (() -> Void)? = nil){
+        isProfileLoaded = false
+        loadProfile(profileUserID: profileUserId) { [weak self] in
+            guard let self = self else { return }
+            self.loadFollowerFollowingCounts(profileUserID: profileUserId)
+            
+            if let loggedInUserID = Auth.auth().currentUser?.uid{
+                self.checkAndSetFollowStatus(loggedInUserID: loggedInUserID, profileUserID: profileUserId)
+            } else {
+                print("User is not logged in")
+            }
+            completion?()
+            
+        }
+    }
 }
+
+
